f5e4768c62d83d42f05240302d02f92e73944851
Robley <robleyadrian@gmail.com>
2018-12-07 10:29:05 +0300

    initial insertion sort.

---
+++ algorithms/sorting/insertion-sort/insertion_sort.py
@@ -0,0 +1,22 @@
+# Insertion sort maintains a sorted sublist in the lower positions of the list.
+# Each new item is then inserted into the previous list in its sorted position.
+# It has a time complexity of O(n^2)
+
+
+def insertion_sort(a_list):
+    for index in range(1, len(a_list)):
+        current_value = a_list[index]
+        position = index
+
+        while position > 0 and a_list[position - 1] > current_value:
+            a_list[position] = a_list[position-1]
+            position = position - 1
+
+        a_list[position] = current_value
+
+    return a_list
+
+
+example_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+
+print(insertion_sort(example_list))  # [17, 20, 26, 31, 44, 54, 55, 77, 93]
