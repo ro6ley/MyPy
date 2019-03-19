c7b17e8aab5d9a95a973ea50fd628bbe38ba5237
Robley <robleyadrian@gmail.com>
2019-03-19 08:59:58 +0300

    add heap sort implementation.

---
+++ algorithms/sorting/heap-sort/heap_sort.py
@@ -0,0 +1,37 @@
+def heapsort(alist):
+    build_max_heap(alist)
+    for i in range(len(alist) - 1, 0, -1):
+        alist[0], alist[i] = alist[i], alist[0]
+        max_heapify(alist, index=0, size=i)
+
+
+def parent(i):
+    return (i - 1)//2
+
+
+def build_max_heap(alist):
+    length = len(alist)
+    start = parent(length - 1)
+    while start >= 0:
+        max_heapify(alist, index=start, size=length)
+        start = start - 1
+
+
+def max_heapify(alist, index, size):
+    l = 2 * index + 1
+    r = 2 * index + 2
+    if (l < size and alist[l] > alist[index]):
+        largest = l
+    else:
+        largest = index
+    if (r < size and alist[r] > alist[largest]):
+        largest = r
+    if (largest != index):
+        alist[largest], alist[index] = alist[index], alist[largest]
+        max_heapify(alist, largest, size)
+
+
+a_list = [12, 11, 13, 5, 6, 7]
+heapsort(a_list)
+print('Sorted list: ')
+print(a_list)
