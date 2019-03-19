d29e931603ab8464c0d0d9378f5ed225913501d1
Robley <robleyadrian@gmail.com>
2019-03-19 10:09:13 +0300

    update docstring.

--- algorithms/sorting/heap-sort/heap_sort.py
+++ algorithms/sorting/heap-sort/heap_sort.py
@@ -1,3 +1,23 @@
+"""
+Heap Sort works by visualizing the elements to be sorted as a heap.
+A heap is a complete binary tree where nodes either have 2 or no kids and each
+    level must be full.
+A Heapify method is used to convert an array into a Max or Min heap.
+Once Heapified, we can identify the largest or the smallest value since it is
+    at the root of the heap.
+This element is put at the end of the array and we can now use the same steps 
+    to find the next element.
+
+Time complexity of Heap Sort is O(n Log n)
+
+NB: For an element `a` in the array at the index a:
+- it's left child is at the index 2 * a + 1
+- it's right child is at the index 2 * a + 2
+- it's parent is at (a -1)//2 
+
+"""
+
+
 def heapsort(alist):
     build_max_heap(alist)
     for i in range(len(alist) - 1, 0, -1):
