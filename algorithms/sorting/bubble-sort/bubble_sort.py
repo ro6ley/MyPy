6c14bdcc0c4461c4cc37f2a2c390c815f4968a2f
Robley <robleyadrian@gmail.com>
2020-02-19 17:10:34 +0300

    refactor swapping operation to rid of temporary variable

--- algorithms/sorting/bubble-sort/bubble_sort.py
+++ algorithms/sorting/bubble-sort/bubble_sort.py
@@ -9,9 +9,7 @@ def bubble_sort(a_list):
     for passnum in range(len(a_list)-1, 0, -1):
         for i in range(passnum):
             if a_list[i] > a_list[i+1]:
-                temp = a_list[i]
-                a_list[i] = a_list[i+1]
-                a_list[i+1] = temp
+                a_list[i], a_list[i+1] = a_list[i+1], a_list[i]
 
     return a_list
 
